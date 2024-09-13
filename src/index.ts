2d8ff142ad299285ddfb6eabbd0cf8d299870116
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-09-13 16:33:44 +0700

    update formatting

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,48 @@
-import type { Simplify, UnionToIntersection } from 'type-fest';
+import type { Simplify, UnionToIntersection } from "type-fest";
 
-/**
- * Represents the options that can be attached to a step.
- */
+/** Represents the options that can be attached to a step. */
 export interface StepOptions {
-  /**
-   * The desired output of the transition to this step.
-   */
+  /** The desired output of the transition to this step. */
   output?: object;
 }
 
-/**
- * Represents any step in a trace.
- */
+/** Represents any step in a trace. */
 export type AnyStep = Step<any, any>;
 
-/**
- * Represents a single step in a trace.
- */
+/** Represents a single step in a trace. */
 export class Step<const Name extends string, Options extends StepOptions> {
-  tag = 'step' as const;
+  tag = "step" as const;
 
   constructor(
     public name: Name,
-    public options: Options
+    public options: Options,
   ) {}
 }
 
-/**
- * Represents a trace of any steps.
- */
+/** Represents a trace of any steps. */
 export type AnyTrace = Trace<[...AnyStep[]]>;
 
-/**
- * Represents a sequence of steps in a trace.
- */
+/** Represents a sequence of steps in a trace. */
 export class Trace<const Steps extends [] | [...AnyStep[]] = []> {
-  tag = 'trace' as const;
+  tag = "trace" as const;
 
   constructor(public steps: Steps = [] as Steps) {}
 
-  static with<Input extends object>(
-    output: Input
-  ): Trace<[Step<'@', { output: Input }>]> {
-    return new Trace([new Step('@', { output })]);
+  static with<Input extends object>(output: Input): Trace<[Step<"@", { output: Input }>]> {
+    return new Trace([new Step("@", { output })]);
   }
 
   step<const Name extends string, Options extends StepOptions>(
     name: Name,
-    options: Options
+    options: Options,
   ): Trace<[...Steps, Step<Name, Options>]> {
     return new Trace([...this.steps, new Step(name, options)]);
   }
 }
 
-/**
- * Represents a transition from one state to another, with input and output types attached.
- */
+/** Represents a transition from one state to another, with input and output types attached. */
 export interface InferredTransition<From, To, Input, Output> {
-  tag: 'inferred transition';
+  tag: "inferred transition";
   from: From;
   to: To;
   input: Input;
@@ -69,86 +53,74 @@ export interface InferredTransition<From, To, Input, Output> {
  * Distributes the list of individual steps into a union of transition pairs.
  *
  * So, for example, a list of steps:
- * - one
- * - two
- * - three
  *
- * is transformed into a list of transitions:
- * - from one to two
- * - from two to three
+ * - One
+ * - Two
+ * - Three
+ *
+ * Is transformed into a list of transitions:
+ *
+ * - From one to two
+ * - From two to three
  */
-export type TraceTransition<
-  Steps extends AnyTrace['steps'],
-  Result = never,
-> = Steps extends [infer From extends AnyStep, ...infer Rest]
+export type TraceTransition<Steps extends AnyTrace["steps"], Result = never> = Steps extends [
+  infer From extends AnyStep,
+  ...infer Rest,
+]
   ? Rest extends [infer To extends AnyStep, ...any]
     ? TraceTransition<
         Rest,
         | Result
         | InferredTransition<
-            From['name'],
-            To['name'],
-            From['options']['output'],
-            To['options']['output']
+            From["name"],
+            To["name"],
+            From["options"]["output"],
+            To["options"]["output"]
           >
       >
     : Result
   : Result;
 
-/**
- * Represents any program.
- */
+/** Represents any program. */
 export type AnyProgram = Program<AnyTrace>;
 
-/**
- * Represents a program that consists of multiple traces.
- */
+/** Represents a program that consists of multiple traces. */
 export class Program<const Trace extends AnyTrace> {
-  tag = 'program' as const;
+  tag = "program" as const;
 
   constructor(public traces: Trace[]) {}
 }
 
-/**
- * Converts a program to a union of transition pairs.
- */
+/** Converts a program to a union of transition pairs. */
 export type InferTransition<T extends AnyProgram> =
-  T extends Program<infer Trace> ? TraceTransition<Trace['steps']> : never;
+  T extends Program<infer Trace> ? TraceTransition<Trace["steps"]> : never;
 
-/**
- * Represents a union of state names that a program can transition from.
- */
-export type FromState<Program extends AnyProgram> =
-  | '*'
-  | InferTransition<Program>['from'];
+/** Represents a union of state names that a program can transition from. */
+export type FromState<Program extends AnyProgram> = "*" | InferTransition<Program>["from"];
 
-/**
- * Represents a union of state names that a program can transition to.
- */
+/** Represents a union of state names that a program can transition to. */
 export type ToState<Program extends AnyProgram, From extends string> =
-  | '*'
+  | "*"
   | (InferTransition<Program> extends {
-      from: From extends '*' ? any : From;
+      from: From extends "*" ? any : From;
       to: infer To;
     }
       ? To
       : never);
 
-/**
- * Represents the output of a transition function.
- */
+/** Represents the output of a transition function. */
 export type FnOutput<Program extends AnyProgram, From, To> =
   InferTransition<Program> extends InferredTransition<
-    From extends '*' ? any : From,
-    To extends '*' ? any : To,
+    From extends "*" ? any : From,
+    To extends "*" ? any : To,
     any,
     infer Output
   >
     ? [
         (
-          | '@'
+          | "@"
           | (InferTransition<Program> extends InferredTransition<
-              To extends '*' ? any : To,
+              To extends "*" ? any : To,
               infer Next,
               any,
               any
@@ -160,18 +132,16 @@ export type FnOutput<Program extends AnyProgram, From, To> =
       ]
     : never;
 
-/**
- * Represents the input of a transition function.
- */
+/** Represents the input of a transition function. */
 export type FnInput<Program extends AnyProgram, From, To> = /*
- */ [From, To] extends ['*', '*']
+ */ [From, To] extends ["*", "*"]
   ? Simplify<
-      UnionToIntersection<InferTransition<Program>['input']> &
-        UnionToIntersection<InferTransition<Program>['output']>
+      UnionToIntersection<InferTransition<Program>["input"]> &
+        UnionToIntersection<InferTransition<Program>["output"]>
     >
   : InferTransition<Program> extends InferredTransition<
-        [From] extends ['*'] ? any : From,
-        [To] extends ['*'] ? any : To,
+        [From] extends ["*"] ? any : From,
+        [To] extends ["*"] ? any : To,
         infer Input,
         any
       >
@@ -179,57 +149,47 @@ export type FnInput<Program extends AnyProgram, From, To> = /*
     : never;
 
 export class Transition {
-  tag = 'transition' as const;
+  tag = "transition" as const;
   constructor(
     public from: string,
     public to: string,
-    public fn: (input: any) => any
+    public fn: (input: any) => any,
   ) {}
 }
 
-/**
- * Represents an implementation of a program.
- */
+/** Represents an implementation of a program. */
 export class Implementation<const Program extends AnyProgram> {
-  tag = 'implementation' as const;
+  tag = "implementation" as const;
 
   constructor(
     public program: Program = new Program([] as any) as Program,
-    public transitions: Transition[] = []
+    public transitions: Transition[] = [],
   ) {}
 
-  transition<
-    const From extends FromState<Program>,
-    const To extends ToState<Program, From>,
-  >(
+  transition<const From extends FromState<Program>, const To extends ToState<Program, From>>(
     from: From & string,
     to: To & string,
-    fn: (input: FnInput<Program, From, To>) => FnOutput<Program, From, To>
+    fn: (input: FnInput<Program, From, To>) => FnOutput<Program, From, To>,
   ) {
-    return new Implementation(
-      this.program,
-      this.transitions.concat(new Transition(from, to, fn))
-    );
+    return new Implementation(this.program, this.transitions.concat(new Transition(from, to, fn)));
   }
 
-  findTransition(from: string, to: string): Transition {
-    const transition = this.transitions.find(
+  getTransition(from: string, to: string) {
+    return this.transitions.find(
       (transition) =>
-        (transition.from === '*' || transition.from === from) &&
-        (transition.to === '*' || transition.to === to)
+        (transition.from === "*" || transition.from === from) &&
+        (transition.to === "*" || transition.to === to),
     );
-
-    if (!transition) throw new Error(`No transition from ${from} to ${to}`);
-
-    return transition;
   }
 
   async run<
-    const From extends InferTransition<Program>['from'],
-    const To extends InferTransition<Program>['to'],
+    const From extends InferTransition<Program>["from"],
+    const To extends InferTransition<Program>["to"],
   >(from: From & string, to: To & string, input: any) {
-    while (to !== '@') {
-      var [next, output] = await this.findTransition(from, to).fn(input);
+    while (to !== "@") {
+      const transition = this.getTransition(from, to);
+      if (!transition) throw new Error(`No transition from ${from} to ${to}`);
+      var [next, output] = await transition.fn(input);
       [from, to] = [to, next];
       input = output;
     }
--- src/spec.ts
+++ src/spec.ts
@@ -1,18 +1,26 @@
-import { Factorial } from './examples/factorial';
-import { Implementation } from './index';
+import { Factorial } from "./examples/factorial";
+import { Implementation } from "./index";
 
-describe('TDS – Test-Driven State', () => {
-  test('factorial example', async () => {
+describe("TDS – Test-Driven State", () => {
+  test("factorial example", async () => {
+    /** The factorial program reference can be used indirectly as a type. */
     const factorial = new Implementation<typeof Factorial>()
-      // reads as: from any state to any state
-      .transition('*', '*', ({ n, a = 1 }) =>
+      /** Reads as: from any state to any state. */
+      .transition("*", "*", ({ n, a = 1 }) =>
         n === 0 //
-          ? ['@', { n: a }]
-          : ['calc', { n: n - 1, a: n * a }]
+          ? ["@", { n: a }]
+          : ["calc", { n: n - 1, a: n * a }],
       );
 
-    expect(await factorial.run('@', 'calc', { n: 0 })).toEqual({ n: 1 });
-    expect(await factorial.run('@', 'calc', { n: 1 })).toEqual({ n: 1 });
-    expect(await factorial.run('@', 'calc', { n: 5 })).toEqual({ n: 120 });
+    /** Knowing an initial transition, we should be able to run the whole program. */
+    expect(await factorial.run("@", "calc", { n: 0 })).toEqual({ n: 1 });
+    expect(await factorial.run("@", "calc", { n: 1 })).toEqual({ n: 1 });
+    expect(await factorial.run("@", "calc", { n: 5 })).toEqual({ n: 120 });
+
+    /**
+     * Knowing an entry state and passing a runtime reference to the factorial program, we can
+     * verify the program against individual traces.
+     */
+    await factorial.verify("@", Factorial);
   });
 });
